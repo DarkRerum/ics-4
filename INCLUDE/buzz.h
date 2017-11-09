199f7c4748e731e755cd940974b610ba3069aebe
gyh <eq>
2017-11-09 16:07:48 +0300

    init commit

---
+++ INCLUDE/buzz.h
@@ -0,0 +1,6 @@
+#ifndef _BUZZ_H
+#define _BUZZ_H
+
+extern void buzz(void);
+
+#endif //_BUZZ_H
---
+++ INCLUDE/mode.h
@@ -0,0 +1,8 @@
+#ifndef __MODE_H
+#define __MODE_H
+
+typedef enum {MODE_CHAR, MODE_NUM} Mode;
+
+extern Mode g_mode;
+
+#endif
